5edbe02a53a02e1dce950ad806b877bbf9474bea
Anri <67234362+anritsetskhla10@users.noreply.github.com>
2025-05-26 02:56:29 +0400

    change app structure

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
+  let plugins = [];
+
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['expo-router/babel'],
+    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+
+    plugins,
   };
 };
