4d6fbabee5383bb217d6ce7e53f0fdadb077c5c6
Anri Tsetskhlashvili <67234362+anritsetskhla10@users.noreply.github.com>
2025-05-28 19:03:53 +0400

    update routing

--- App.tsx
+++ App.tsx
@@ -1,14 +1,1 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
-import './global.css';
-
-export default function App() {
-  return (
-    <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <ScreenContent title="Auth" path="pages/Auth.tsx"></ScreenContent>
-      <StatusBar style="auto" />
-    </>
-  );
-}
+import "expo-router/entry";
